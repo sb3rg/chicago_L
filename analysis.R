1ba602002a09251fae3ac82791b96570ddda22e6
Sean Bergstedt <seanbergstedt@gmail.com>
2018-02-05 22:56:32 -0600

    small corrections to std deviation result based on check

--- analysis.R
+++ analysis.R
@@ -38,7 +38,7 @@ keycols <- c( "stationname", "DATE", "daytype" )
 sum_by_uniq <- copy[, .( sum_rides = rides %>% sum() ), by = keycols ]
 
 ##calculate rideship per day by station
-avg_by_stn <- sum_by_uniq[, .( avg_rides = sum_rides %>% mean ), by = stationname ] %>%
+avg_by_stn <- sum_by_uniq[ sum_rides > 0 , .( avg_rides = sum_rides %>% mean ), by = stationname ] %>%
   ##sort in descending order
   (function(x) x[order(-avg_rides )])
   
@@ -54,16 +54,20 @@ avg_by_stn <- sum_by_uniq[, .( avg_rides = sum_rides %>% mean ), by = stationnam
 
 ####QUESTION 2:  Which stop has the greatest standard deviation in 
 # weekday (exclude holidays) ridership per day, and what is it?
-std_dev_by_stn <- sum_by_uniq[ daytype == "W" ] %>% #filter out weekends and holidays
+std_dev_by_stn <- sum_by_uniq[ daytype == "W" & sum_rides > 0 ] %>% #filter out weekends and holidays
   ##calculate std_dev
   (function(x) x[, .( std_dev_rides = sum_rides %>% sd ), by = stationname]) %>%
   ##sort in descending order
   (function(x) x[order(-std_dev_rides)])
 
 ##print results
-"The greatest standard devation of weekday ridership is for station: " %>% 
+"The greatest standard deviation of weekday ridership is for station: " %>% 
   paste0( std_dev_by_stn[1]$stationname %>% sQuote(), 
           " with a standard deviation of ",
           std_dev_by_stn[1]$std_dev_rides %>% as.integer(),
           " boardings per day" ) %>%
   print()
+
+
+
+####QUESTION 3: Choose a business
